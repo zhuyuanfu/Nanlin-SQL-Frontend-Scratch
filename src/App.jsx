12b6fa12f2351ccb15184fd5ecacbffe43e8ec98
Zhu Yuanfu <641561609@qq.com>
2022-11-20 23:17:14 +0800

    +[frontend: added css support; adjusted layout]

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Button } from 'antd';
 import OverallLayout from './component/layout/OverallLayout.jsx';
 const App = () => {
   return <div>
-      <h1>Hello World! hahahah</h1>
       <OverallLayout />
     </div>;
 };
--- src/component/layout/OverallLayout.jsx
+++ src/component/layout/OverallLayout.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Breadcrumb, Button, Layout } from 'antd';
 import { Menu } from "antd";
+
+import './OverallLayout.css'
+
 const { Header, Sider, Content } = Layout;
 
 const navigationItems = ['1', '2', '3'].map((key) => (
@@ -13,33 +16,37 @@ const navigationItems = ['1', '2', '3'].map((key) => (
 class OverallLayout extends React.Component {
     render () {
         return (
-            <Layout> 
-                <Header className="header" >
-                    <div className="logo" color="white">njfu</div>
-                    {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} /> */}
-                </Header>
-                <Layout>
-                    <Sider width={200} className="site-layout-background">
-                        <Button >place left tree here</Button>
-                    </Sider>
+            <div>
+                <Layout> 
+                    <Header className="header" >
+                        <div className="logo" >njfu</div>
+
+                        {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} /> */}
+                    </Header>
+                    <Layout>
+                        <Sider width={150} className="site-layout-background">
+                            <Button >place left tree here</Button>
+                        </Sider>
+                        <Layout style={{padding: '0 24px 24px'}}>
+                            <Breadcrumb style={{margin: '16px 0'}}>
+                                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                                <Breadcrumb.Item>List</Breadcrumb.Item>
+                                <Breadcrumb.Item>App</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <Content
+                                className="site-layout-background"
+                                style={{
+                                    padding: 24,
+                                    margin: 0,
+                                    minHeight: 280
+                                }}
+                            >
+                                <Button >place content here</Button>
+                            </Content>
+                        </Layout>
+                    </Layout>
                 </Layout>
-                <Layout style={{padding: '0 24px 24px'}}>
-                    <Breadcrumb style={{margin: '16px 0'}}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <Content
-                        style={{
-                            padding: 24,
-                            margin: 0,
-                            minHeight: 280
-                        }}
-                    >
-                        <Button >place content here</Button>
-                    </Content>
-                </Layout>
-            </Layout>
+            </div>
         )
     }
 }
